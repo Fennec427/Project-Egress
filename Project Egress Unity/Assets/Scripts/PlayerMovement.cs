03fc0d938a796995e91b1c493aa1a3aab16a05dd
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-03-05 20:33:21 -0500

    Simple left-right movement + file cleanup You're welcome Maddie

--- Project Egress Unity/Assets/Scripts/PlayerMovement.cs
+++ Project Egress Unity/Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float horizontalInput;
+    public InputAction move;
     float movespeed = 5f;
     Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        move = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 moveValue = move.ReadValue<Vector2>();
+        //print(moveValue);
+        rb.linearVelocity = new Vector2(moveValue.x*movespeed, rb.linearVelocity.y);
 
     }
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontalInput*movespeed, rb.linearVelocity.y);
+        //rb.linearVelocity = new Vector2(horizontalInput*movespeed, rb.linearVelocity.y);
+        
     }
 }
