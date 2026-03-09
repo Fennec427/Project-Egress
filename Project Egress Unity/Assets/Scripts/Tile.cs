34cfa4d6adde37dec172bedaa2246fbd971822bb
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-03-09 12:19:22 -0400

    File cleanup + start prefab work

---
+++ Project Egress Unity/Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+public class Tile : MonoBehaviour
+{
+    public Sprite[] tiles;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int rand = Random.Range(0,tiles.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
