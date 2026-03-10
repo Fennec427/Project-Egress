e291f7c5e7b76e032d16a3d2b753cb160d131fec
Fennec427 <Fennec427@users.noreply.github.com>
2026-03-10 06:59:56 -0700

    Jump functionality edit!

--- Project Egress Unity/Assets/Scripts/PlayerMovement.cs
+++ Project Egress Unity/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,20 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public InputAction move;
-    float movespeed = 5f;
+    public InputAction jump; //N - J
+       [SerializeField] float movespeed = 5f; // Just Added serialize field  technically N - J
+        [SerializeField] float jumpForce = 10f; // Force applied when jumping N - J
     Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         move = InputSystem.actions.FindAction("Move");
+        jump = InputSystem.actions.FindAction("Jump"); // N - J
+
+        move.Enable(); // N - J
+        jump.Enable(); // N - J
+
     }
 
     // Update is called once per frame
@@ -19,6 +26,14 @@ public class PlayerMovement : MonoBehaviour
         Vector2 moveValue = move.ReadValue<Vector2>();
         //print(moveValue);
         rb.linearVelocity = new Vector2(moveValue.x*movespeed, rb.linearVelocity.y);
+
+         // 2. Jump Logic - Both comment are N - J
+        // WasPressedThisFrame is ideal for jumping so it only triggers once per tap
+        if (jump.WasPressedThisFrame()) //All stuff from here to 4 lines below is N - J
+        {
+            // Simple Jump: sets the vertical velocity directly
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }       
 
     }
     private void FixedUpdate()
