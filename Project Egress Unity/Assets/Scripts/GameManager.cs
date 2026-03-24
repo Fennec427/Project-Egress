05dc8baaa5e717a649df6194e70a89e3f33a4410
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-03-24 12:16:52 -0400

    Add manual painting, paint sprites

--- Project Egress Unity/Assets/Scripts/GameManager.cs
+++ Project Egress Unity/Assets/Scripts/GameManager.cs
@@ -1,14 +1,27 @@
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject Player;
     [SerializeField] Transform Respawn;
+    
+    private InputAction jump;
+    public Tilemap map;
+    public TileBase toPlace;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        jump = InputSystem.actions.FindAction("Jump");
+    }
+    void Update()
+    {
+        if (jump.WasPressedThisFrame())
+        {
+            map.SetTile(new Vector3Int(1,1,1), toPlace);
+        }
     }
 
     // Update is called once per frame
