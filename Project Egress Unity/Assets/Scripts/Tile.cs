5c4cf78f159db5ab63b840a6fa699fe35c6fee8f
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-04-16 12:12:24 -0400

    Begin fixing tile bumps

--- Project Egress Unity/Assets/Scripts/Tile.cs
+++ Project Egress Unity/Assets/Scripts/Tile.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public Sprite[] tiles;
+    [Header("Tile Detection")]
+    [SerializeField] private GameObject[] detectors;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float extendAmt = 0.1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Randomization of sprites
         int rand = Random.Range(0, 101);
         if(rand >= 0 && rand <= 90)
         {
@@ -19,6 +24,39 @@ public class Tile : MonoBehaviour
         if(rand > 95 && rand <= 100)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = tiles[2]; // Rusted tile
+        }
+
+        // Extend hitbox to overlap on other tiles
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        for(int i = 0; i < detectors.Length; i++)
+        {
+            if(!Physics2D.OverlapCircle(detectors[i].transform.position, 0.07f, groundLayer))
+            {
+                print("false");
+            }
+            if(Physics2D.OverlapCircle(detectors[i].transform.position, 0.07f, groundLayer)) // if another tile is detected
+            {
+                if(i == 0) // top
+                {
+                    collider.size = new Vector2(collider.size.x, collider.size.y + extendAmt);
+                    collider.offset = new Vector2(collider.offset.x, collider.offset.y + (extendAmt / 2f));
+                }
+                else if(i == 1) // right
+                {
+                    collider.size = new Vector2(collider.size.x + extendAmt, collider.size.y);
+                    collider.offset = new Vector2(collider.offset.x + (extendAmt / 2f), collider.offset.y);
+                }
+                else if(i == 2) // bottom
+                {
+                    collider.size = new Vector2(collider.size.x, collider.size.y + extendAmt);
+                    collider.offset = new Vector2(collider.offset.x, collider.offset.y - (extendAmt / 2f));
+                }
+                else if(i == 3) // left
+                {
+                    collider.size = new Vector2(collider.size.x + extendAmt, collider.size.y);
+                    collider.offset = new Vector2(collider.offset.x - (extendAmt / 2f), collider.offset.y);
+                }
+            }
         }
     }
 }
