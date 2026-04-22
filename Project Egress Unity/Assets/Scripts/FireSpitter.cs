b3c5e89f2afd6ceabe2e3d19fcacced966be1507
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-04-22 12:16:06 -0400

    Finish animating Fire Spitter

---
+++ Project Egress Unity/Assets/Scripts/FireSpitter.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class FireSpitter : MonoBehaviour
+{
+    private float idleTime;
+    [SerializeField] private float idleWait = 5f;
+    private float warnTime;
+    [SerializeField] private float warnWait = 2f;
+    private float activeTime;
+    [SerializeField] private float activeWait = 3f;
+    private float deactivateTime;
+    [SerializeField] private float deactivateWait = 1f;
+    public Animator animator;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        idleTime = idleWait;
+        warnTime = 0f;
+        activeTime = 0f;
+        deactivateTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(idleTime <= 0f && animator.GetInteger("currentState") == 0)
+        {
+            animator.SetTrigger("warn");
+            warnTime = warnWait;
+            animator.SetInteger("currentState", 1);
+        }
+        else if(warnTime <= 0f && animator.GetInteger("currentState") == 1)
+        {
+            animator.SetTrigger("active");
+            activeTime = activeWait;
+            animator.SetInteger("currentState", 2);
+            //start kill
+        }
+        else if(activeTime <= 0f && animator.GetInteger("currentState") == 2)
+        {
+            animator.SetTrigger("deactivate");
+            deactivateTime = deactivateWait;
+            animator.SetInteger("currentState", 3);
+            //end kill
+        }
+        else if(deactivateTime <= 0f && animator.GetInteger("currentState") == 3)
+        {
+            animator.SetTrigger("idle");
+            idleTime = idleWait;
+            animator.SetInteger("currentState", 0);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        idleTime -= Time.deltaTime;
+        warnTime -= Time.deltaTime;
+        activeTime -= Time.deltaTime;
+        deactivateTime -= Time.deltaTime;
+    }
+}
