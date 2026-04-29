27dc923475ffcc2e016bb96352c80481869b6266
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-04-29 12:17:12 -0400

    Update Teleporters

--- Project Egress Unity/Assets/Scripts/Teleporter.cs
+++ Project Egress Unity/Assets/Scripts/Teleporter.cs
@@ -7,12 +7,13 @@ public class Teleporter : MonoBehaviour
     public float teleportDelay = 1.0f; //Seconds between each teleport
     [Range(0.1f, 1.0f)] 
     public float topDetectionThreshold = 0.5f; //Top collision for telporting
-    private static float lastTeleportTime; // Static so it can apply to all teleporters 
-     
+    private static float lastTeleportTime = 0f; // Static so it can apply to all teleporters 
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player") && Time.time > lastTeleportTime + teleportDelay)
         {
+            /*
             //Meant to check if the player collision is above the teleporter 
             Vector2 contactPoint = collision.transform.position; 
             Vector2 teleporterPoint = transform.position;  
@@ -22,6 +23,8 @@ public class Teleporter : MonoBehaviour
             {
                 TeleportPlayer(collision.gameObject);
             }
+            */
+            TeleportPlayer(collision.gameObject);
         }
     } 
      
