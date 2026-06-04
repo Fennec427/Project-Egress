6dcec7734f785bf083031317d4e1ba64c3049135
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-06-04 11:39:11 -0400

    Paint gun rotates around player

--- Project Egress Unity/Assets/Scripts/PaintGun.cs
+++ Project Egress Unity/Assets/Scripts/PaintGun.cs
@@ -15,13 +15,10 @@ public class PaintGun : MonoBehaviour
     void Update()
     {
         Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
-        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 10f));
-        Vector2 mouseRelativePos = parent.transform.InverseTransformPoint(worldMousePos);
-        
-        //set parent rotation to point at mouse
-        //Vector2 direction = (mouseRelativePos - (Vector2)parent.transform.position).normalized;
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        //parent.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        parent.transform.LookAt(worldMousePos);
+        Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+
+        Vector2 direction = worldMousePos - parent.transform.position;
+        Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, direction);
+        parent.transform.rotation = Quaternion.Slerp(parent.transform.rotation, targetRotation, 10f * Time.deltaTime);
     }
 }
