f0b510abe14dc1b5b76aec4fdbb099cfd67a9d78
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-06-04 12:12:49 -0400

    Flip paint gun sprite based on rotation

--- Project Egress Unity/Assets/Scripts/PaintGun.cs
+++ Project Egress Unity/Assets/Scripts/PaintGun.cs
@@ -5,10 +5,13 @@ public class PaintGun : MonoBehaviour
 {
     GameObject parent;
     [SerializeField] Sprite[] sprites;
+    SpriteRenderer sr;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         parent = transform.parent.gameObject;
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,5 +23,9 @@ public class PaintGun : MonoBehaviour
         Vector2 direction = worldMousePos - parent.transform.position;
         Quaternion targetRotation = Quaternion.FromToRotation(Vector3.up, direction);
         parent.transform.rotation = Quaternion.Slerp(parent.transform.rotation, targetRotation, 10f * Time.deltaTime);
+
+        print(parent.transform.eulerAngles.z);
+        if(parent.transform.rotation.eulerAngles.z > 0 && parent.transform.rotation.eulerAngles.z < 180) sr.flipY = true;
+        else sr.flipY = false;
     }
 }
