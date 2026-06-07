b3338f429c8ee238c7c31ebc7538513cd0034e21
AttakDoge <63970527+AttakDoge@users.noreply.github.com>
2026-06-07 19:39:25 -0400

    More orange tile fixes

--- Project Egress Unity/Assets/Scripts/Buzzsaw Rotation.cs
+++ Project Egress Unity/Assets/Scripts/Buzzsaw Rotation.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class BuzzsawRotation : MonoBehaviour
 {
-    [SerializeField] private float speed = -100f; 
+    [SerializeField] private float speed = -100f;
+    private PolygonCollider2D col;
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        col = GetComponent<PolygonCollider2D>();
+    }
 
     void Update()
     {
