48cbb1b09fe79233a6811ccabced71327a7b5737
FlashElo <madeline.trans.girl@gmail.com>
2026-06-11 10:47:35 +1000

    Foidian removal

--- Project Egress Unity/Assets/Scripts/PlayerMovement.cs
+++ Project Egress Unity/Assets/Scripts/PlayerMovement.cs
@@ -309,7 +309,7 @@ public class PlayerMovement : MonoBehaviour
                                 movementDisabled = colTile.MovementDisableTime;
                                 DisableJumps(colTile.MovementDisableTime, true);
                             }
-                            greenTileTime = 0.50f;
+                            greenTileTime = 0.25f;
                         }
                     }
                     
@@ -425,7 +425,7 @@ public class PlayerMovement : MonoBehaviour
                     movementDisabled = tile.MovementDisableTime;
                     DisableJumps(tile.MovementDisableTime, true);
                 }
-                greenTileTime = 0.50f;
+                greenTileTime = 0.25f;
             }
         }
     }
